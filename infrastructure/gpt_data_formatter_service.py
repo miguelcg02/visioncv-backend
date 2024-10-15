6b3a0d00f1d309c36cdcb3387095b37986de63f7
Adrephos <adrephos@gmail.com>
2024-10-14 20:10:44 -0500

    refactor: make data formatter function usable for any cv section

--- infrastructure/gpt_data_formatter_service.py
+++ infrastructure/gpt_data_formatter_service.py
@@ -11,20 +11,16 @@ class GPTDataFormatterService(DataFormatterService):
     client: OpenAI
     model: str = field(default="gpt-3.5-turbo")
 
-    def __get_experience_array(self, experience: str) -> List[Dict[str, str]]:
+    def __format_data(
+            self, task: str, data_format: str, data: str) -> Dict:
         prompt = (
-            "Eres un asistente de recursos humanos y tienes que "
-            "transcribir la experiencia laboral de un candidato:\n\n"
+            f"{task}:\n\n"
             "Deberás responder solo en formato JSON con los siguientes "
-            "campos:\nexperiencias: array de experiencias\n\n"
-            "Cada experiencia cuenta con los siguientes datos:\n"
-            "date: Fecha de inicio y fin del trabajo\n"
-            "name: Nombre de la empresa\n"
-            "place: Lugar de trabajo\n"
-            "description: Descripción del trabajo\n\n"
-            "A continuación la experiencia laboral del candidato:\n"
+            "campos:\n"
+            f"{data_format}\n\n"
+            "Esta es la información que debes transcribir:\n\n"
             "\"\"\"\n"
-            f"{experience}"
+            f"{data}"
             "\"\"\"\n"
         )
 
@@ -38,17 +34,26 @@ class GPTDataFormatterService(DataFormatterService):
         response = completion.choices[0].message.content
 
         if not response:
-            return []
+            return {}
 
         try:
             response = response.replace("```json", "").replace("```", "")
-            return json.loads(response)["experiencias"]
+            return json.loads(response)
         except Exception as e:
-            raise ValueError("Error parsing experience") from e
+            raise ValueError("Error parsing data") from e
 
     def create_experience_section(
             self, experience: str) -> List[CVDateNamePlaceField]:
-        experience_array = self.__get_experience_array(experience)
+        experience_array = self.__format_data(
+            task=("Eres un asistente de recursos humanos y tienes que "
+                  "transcribir la experiencia laboral de un candidato"),
+            data_format=("campos:\nexperiencias: array de experiencias\n\n"
+                         "Cada experiencia cuenta con los siguientes datos:\n"
+                         "date: Fecha de inicio y fin del trabajo\n"
+                         "name: Nombre de la empresa\n"
+                         "place: Lugar de trabajo\n"
+                         "description: Descripción del trabajo\n\n"),
+            data=experience)
         return [
             CVDateNamePlaceField(
                 date=exp["date"],
@@ -56,5 +61,5 @@ class GPTDataFormatterService(DataFormatterService):
                 place=exp["place"],
                 description=exp["description"]
             )
-            for exp in experience_array
+            for exp in experience_array["experiencias"]
         ]
