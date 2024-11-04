696c5aafbb8186f20d0ab98b157fa98e1d62200b
Adrephos <adrephos@gmail.com>
2024-11-03 20:13:16 -0500

    feat: add file for dependency injection

--- api/routes.py
+++ api/routes.py
@@ -1,40 +1,15 @@
-import os
-from dotenv import load_dotenv
-from openai import AzureOpenAI
 from fastapi import APIRouter, File, UploadFile, Response, status
+from api.di import get_generate_cv_use_case, whisper_stt_service
 from application.dtos.form_dto import FormDTO
-from application.generate_cv import GenerateCV
-from infrastructure.whisper_stt_service import WhisperSTTService
-from infrastructure.pdf_cv_generator_service import PdfCVGeneratorService
-from infrastructure.gpt_data_formatter_service import GPTDataFormatterService
 
 
 router = APIRouter()
-
-load_dotenv()
-azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
-open_ai_client = AzureOpenAI(
-    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
-    api_version="2024-02-01",
-    azure_endpoint=azure_endpoint if azure_endpoint else ""
-)
-deployment = os.getenv("AZURE_OPENAI_DEPLOYMENT")
-
-gpt_data_formatter_service = GPTDataFormatterService(
-    open_ai_client, deployment if deployment else "gtp-4o")
-whisper_stt_service = WhisperSTTService(open_ai_client)
-pdf_cv_generator_service = PdfCVGeneratorService()
 
 
 @router.post("/form/upload")
 async def upload_form(form_dto: FormDTO):
     try:
-        generate_cv_use_case = GenerateCV(
-            form_dto,
-            whisper_stt_service,
-            gpt_data_formatter_service,
-            pdf_cv_generator_service
-        )
+        generate_cv_use_case = get_generate_cv_use_case(form_dto)
 
         return {"success": True,
                 "data": {"cv_path": generate_cv_use_case.generate()}}
